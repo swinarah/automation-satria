8f9c58958dee82a368ba3991226178313b102d46
swinarah <satria.winarah@sawitpro.com>
2023-06-14 18:03:10 +0700

    init

---
+++ src/main/java/com/dsp/automation/AutomationSatriaApplication.java
@@ -0,0 +1,13 @@
+package com.dsp.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationSatriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationSatriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsp/automation/AutomationSatriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsp.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationSatriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
